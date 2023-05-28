02067654bdbfb7afade211d919e7d5b1eb367549
KhaledEllithy <khaledellessy310@gmail.com>
2023-05-28 20:49:27 +0300

    Adding Sign In And Register Forms With Validation In Frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { SignInComponent } from "./register/components/sign-in/sign-in.component";
+import { SignUpComponent } from "./register/components/sign-up/sign-up.component";
+import { NotFoundPageComponent } from "./shared/components/not-found-page/not-found-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "signin",
+    component: SignInComponent,
+  },
+  {
+    path: "signup",
+    component: SignUpComponent,
+  },
+  {
+    path: "**",
+    component: NotFoundPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { RegisterModule } from './register/register.module';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { SignInComponent } from './register/components/sign-in/sign-in.component';
+import { SignUpComponent } from './register/components/sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SignInComponent, SignUpComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    // RegisterModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
