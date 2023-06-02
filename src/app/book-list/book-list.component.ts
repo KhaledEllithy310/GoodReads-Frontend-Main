e68963a35f18c79b8f24ce4f25d50e6031b58a89
KhaledEllithy <khaledellessy310@gmail.com>
2023-06-02 21:44:45 +0300

    Change Style Sign UP Form

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -10,9 +10,9 @@ import { Book } from './../interfaces/book';
 })
 export class BookListComponent {
 
-  books : Book[] = [] 
+  books : Book[] = []
   constructor(public router: Router,private GetdataService :GetdataService ){}
-  redirectDetails(id:number){
+  redirectDetails(id:any){
     this.router.navigate(['book-details',id])
   }
 
@@ -23,9 +23,9 @@ export class BookListComponent {
     this.GetdataService .getAllBook().subscribe((res:any)=>{
     this.books = res.response;
     this.totalLength =res.response.length;
-      
+
     })
-  
+
   }
   ngOnInit(){
     this.getAllBook()
