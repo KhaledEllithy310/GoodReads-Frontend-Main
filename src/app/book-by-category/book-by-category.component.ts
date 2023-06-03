ef1982217066e4d76d0c9f6473eabf56ea1cfd17
KhaledEllithy <khaledellessy310@gmail.com>
2023-06-04 01:00:22 +0300

    Style For First Section Of Home Page && Adding Style Category Menu And Routes

--- src/app/book-by-category/book-by-category.component.ts
+++ src/app/book-by-category/book-by-category.component.ts
@@ -6,29 +6,48 @@ import { GetdataService } from './../services/getdata.service';
 @Component({
   selector: 'app-book-by-category',
   templateUrl: './book-by-category.component.html',
-  styleUrls: ['./book-by-category.component.css']
+  styleUrls: ['./book-by-category.component.css'],
 })
 export class BookByCategoryComponent {
   targetBooks!: any;
   BooksOfCategory: Book[] = [];
+  // Category: Book[] = [];
   constructor(
     private activatedRoute: ActivatedRoute,
     private GetdataService: GetdataService
   ) {}
   ngOnInit() {
+    this.getBookByCategory(this.activatedRoute.snapshot.params['id']);
     console.log(this.activatedRoute.snapshot.params['id']);
     this.targetBooks = this.BooksOfCategory.find(
       (category) => category.id == this.activatedRoute.snapshot.params['id']
     );
-    console.log(this.activatedRoute.snapshot.params['id']);
+    this.getAllBook();
   }
+
   totalLength: any;
   p: number = 1;
   itemsPerPage: number = 8;
+
+  // getBookByCategory(id:any) {
+  //   this.GetdataService.getBookByCategory(id).subscribe((res: any) => {
+  //     this.BooksOfCategory = res.response;
+  //     this.totalLength = res.response.length;
+  //   });
+  // }
+
+  getBookByCategory(id: any) {
+    this.GetdataService.getBookByCategory(id).subscribe((res: any) => {
+      console.log(res);
+    });
+  }
+
   getAllBook() {
-    this.GetdataService.getAllBook().subscribe((res: any) => {
-      this.BooksOfCategory = res.response;
-      this.totalLength = res.response.length;
+    return this.GetdataService.getAllBook().subscribe((res: any) => {
+      this.BooksOfCategory = res.response.filter(
+        (elem: any) =>
+          elem.categoryId._id == this.activatedRoute.snapshot.params['id']
+      );
     });
   }
 }
