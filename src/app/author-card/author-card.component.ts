55973146b2548bd7311ee056ebe4e37e5194c0c3
KhaledEllithy <khaledellessy310@gmail.com>
2023-06-04 11:50:28 +0300

    Adding Redirect To Author Details Page

--- src/app/author-card/author-card.component.ts
+++ src/app/author-card/author-card.component.ts
@@ -1,21 +1,21 @@
-import { Component,Input ,Output,EventEmitter} from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Author } from './../interfaces/author';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-author-card',
   templateUrl: './author-card.component.html',
-  styleUrls: ['./author-card.component.css']
+  styleUrls: ['./author-card.component.css'],
 })
 export class AuthorCardComponent {
+  @Input() card!: any;
+  @Output() emitFromChild = new EventEmitter();
 
-  @Input() card !: any;
-  @Output() emitFromChild = new EventEmitter()
-  constructor(private router: Router){}
-  redirectDetails(id : any){
-    this.router.navigate(['author-details'])
- 
+  constructor(private router: Router) {}
+
+  //REDIRECT TO AUTHOR DETAILES PAGE
+  redirectDetails(id: any) {
+    this.router.navigate(['author-details', id]);
     console.log(this.card.avatar);
-    
     // this.emitFromChild.emit(id)
-}
+  }
 }
